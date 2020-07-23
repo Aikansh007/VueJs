c7999b635532e48468264c433f6b13ec10476a4a
Aikansh Tiwari <Aikansh.Tiwari@igglobal.com>
2020-07-23 15:45:12 +0530

    project initialization

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <div class="row">
+            <div class="col-xs-12">
+                <transition name="slide" mode="out-in">
+        <router-view></router-view>
+                </transition>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Header.vue';
+    export default {
+        components : {
+            appHeader : Header
+        },
+        created(){
+            this.$store.dispatch('initStocks')
+        }
+
+    }
+</script>
+
+<style>
+body{
+    padding: 30px;
+}
+
+ /* sliding-------------------------------------------------------------- */
+    .slide-enter{
+        opacity: 0;
+    }
+    .slide-enter-active{
+        animation: slide-in 300ns ease-out forwards;
+        transition: opacity .5s;
+    }
+   
+    .slide-leave-active{
+          animation: slide-out 300ns ease-out forwards;
+           transition: opacity 1s;
+           opacity: 0;
+           position: absolute;
+    }
+    .slide-move{
+        transition: transform 1s;
+    }
+    /* ---------------------------------------------------------------------- */
+
+    @keyframes slide-in {
+      from{
+        transform: translateY(20px);
+      }
+      to{
+        transform: translateY(0);
+      }
+    }
+    @keyframes slide-out{
+       from{
+        transform: translateY(0);
+      }
+      to{
+        transform: translateY(20px);
+      }
+    }
+</style>
